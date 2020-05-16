9affab5efb9b52ba2abc5193fcefcb6f61b58e82
pedromolina <pedro@aspgems.com>
2020-05-16 17:36:58 +0200

    add async, json, custom and date

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   nombre: string = 'Capitán América';
+  nombre2: string = 'PeDrO MoLInA';
   arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   PI: number = Math.PI;
   porcentaje: number = 0.234;
   salario: number = 1234.5;
+
+  valorPromesa = new Promise<string>((resolve) => {
+    setTimeout(() => {
+      resolve('llegó la data');
+    }, 4500);
+  })
+  fecha: Date = new Date;
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 500,
+    direccion: {
+      calle: 'primera',
+      casa: 20
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+registerLocaleData(localeEs);
 import { AppComponent } from './app.component';
+import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapitalizadoPipe
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'es'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
